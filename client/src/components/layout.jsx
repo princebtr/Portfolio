c24518650cb1f93ed69b2dfb870a591bd3fcd6e1
princebtr <princekumar07014@gmail.com>
2025-06-14 16:57:54 +0530

    first commit

---
+++ client/src/components/layout.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import Home from "../pages/home";
+import {
+  TbArrowBigLeftLinesFilled,
+  TbArrowBigRightLinesFilled,
+} from "react-icons/tb";
+import "./layout.css";
+import Menu from "./menu";
+
+const layout = () => {
+  const [toggle, setToggle] = useState(true);
+  //   change toggle
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+  return (
+    <>
+      <div className="sidebar-section">
+        <div className={toggle ? "sidebar-toggle" : "sidebar"}>
+          <div className="icon">
+            <Menu />
+            <p onClick={handleToggle}>
+              {toggle ? (
+                <TbArrowBigLeftLinesFilled size={30} />
+              ) : (
+                <TbArrowBigRightLinesFilled size={30} />
+              )}
+            </p>
+          </div>
+        </div>
+        <div className="container">
+          <Home />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default layout;
---
+++ client/src/components/menu.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./menu.css";
+
+function menu() {
+  return (
+    <>
+      <div className="profile-pic">
+        <img
+          src="https://www.w3schools.com/howto/img_avatar.png"
+          alt="Profile"
+        />
+      </div>
+    </>
+  );
+}
+
+export default menu;
